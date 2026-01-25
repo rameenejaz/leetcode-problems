adf26c63b4bcf92f8423a33a67e73c578b464514
Rameen Ejaz <rameenejaz4@gmail.com>
2026-01-25 23:44:12 +0500

    adding two numbers such that they meet the targe t

---
+++ twoSum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class twoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int needed = target - nums[i];
+            
+            if (map.containsKey(needed)) {
+                return new int[]{map.get(needed), i};
+            }
+            
+            map.put(nums[i], i);
+        }
+        return new int[]{}; // guaranteed one solution, so this won't run
+    }
+}
