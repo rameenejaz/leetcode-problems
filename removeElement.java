044ca93b7f09009a3de02b5dd0284667c8c27fd9
Rameen Ejaz <rameenejaz4@gmail.com>
2026-01-30 23:32:49 +0500

    removing a specific element

---
+++ removeElement.java
@@ -0,0 +1,13 @@
+class removeElement {
+    public int removeElement(int[] nums, int val) {
+        int k = 0; // index for elements not equal to val
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
