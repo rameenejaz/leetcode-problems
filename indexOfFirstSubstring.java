bc79c664923487f4525a813df217d56ed86d4f73
Rameen Ejaz <rameenejaz4@gmail.com>
2026-02-03 23:36:53 +0500

    removing a specific element

--- indexOfFirstSubstring.java
+++ indexOfFirstSubstring.java
@@ -1,9 +1,7 @@
 // Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 
-
 class indexOfFirstSubstring {
-public
-    int strStr(String haystack, String needle) {
+public int strStr(String haystack, String needle) {
         int hl=haystack.length();
         int nl=needle.length();
         if (nl>hl) {
