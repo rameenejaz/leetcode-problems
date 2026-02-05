67aabb240da4e32091920174f1bbe48bb733118b
Rameen Ejaz <rameenejaz4@gmail.com>
2026-02-05 23:34:42 +0500

    checking for palindrome numbers

---
+++ isPalindrome.java
@@ -0,0 +1,19 @@
+class isPalindrome {
+public boolean isPalindrome(int x) {
+        // negative numbers or numbers ending with 0 (except 0 itself)
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversedHalf = 0;
+
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x = x / 10;
+        }
+
+        // for even digits: x == reversedHalf
+        // for odd digits: x == reversedHalf / 10
+        return (x == reversedHalf || x == reversedHalf / 10);
+    }
+};
